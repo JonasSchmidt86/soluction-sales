2dfb94d2f08ccdcea2c2e191d9c3d3c553e9ef07
JonasSchmidt86 <122678522+JonasSchmidt86@users.noreply.github.com>
2025-02-28 12:29:24 -0300

    melhorias

--- config/application.rb
+++ config/application.rb
@@ -47,4 +47,12 @@ module SoluctionSales
 
   end
 
+  config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins 'https://www.moveisrosa.shop'
+      resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete, :options, :head], credentials: true
+    end
+  end
+
+  
 end
